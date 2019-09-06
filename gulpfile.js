eafe7d3522e6f9234a8b8027f25e0f2f3a98f81c
Giacomo Parmeggiani <giacomo.parmeggiani@gmail.com>
2019-09-06 16:14:50 +0200

    Revert "Fix minor bug in the clean task"
    This reverts commit af1fd9ec17e3add75488f997803bea90bcd74579.

--- gulpfile.js
+++ gulpfile.js
@@ -37,10 +37,10 @@ const cstyle_banner = ['/*!\n',
 /**
  * Delete the contents of the www (output) directory
  */
-function clean(cb) {
-    del([
+function clean() {
+    return del([
         'www/**/*'
-    ], cb);
+    ]);
 }
 
 /**
